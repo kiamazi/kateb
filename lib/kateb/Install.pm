38f15ff64ac4219a06d4cb57613d46ce09e2fd81
kiavash <kiavash@gmail.com>
2020-03-17 11:45:33 +0330

    bug fix

--- lib/kateb/Install.pm
+++ lib/kateb/Install.pm
@@ -40,7 +40,7 @@ sub new {
 			errlist => [],
 			fonts   => '',
 		};
-	} elsif ($args[0] =~ /all|-a/) {
+	} elsif ($args[0] =~ /^all$|^-a$/) {
 		$self ={
 			error   => 0,
 			message => "ok!",
@@ -285,7 +285,7 @@ sub _unzip {
 			$zip->extractMember( $file, catfile($cache_dir, $file_name) );
 			push @extracted_fonts, catfile($cache_dir, $file_name);
 		}
-	} elsif ($font_name =~ /ganjnameh|nika|mikhak/)
+	} elsif ($font_name =~ /^ganjnameh$|^nika$|^mikhak$/)
 	{
 		foreach my $file (grep { m{.*\.ttf$}g } $zip->memberNames())
 		{
@@ -293,7 +293,7 @@ sub _unzip {
 			$zip->extractMember( $file, catfile($cache_dir, $file_name) );
 			push @extracted_fonts, catfile($cache_dir, $file_name);
 		}
-	} elsif ($font_name =~ /pfont/)
+	} elsif ($font_name =~ /^pfont$/)
 	{
 		foreach my $file (grep { m{(?<!__MACOSX\/)pfont/ttf/Hinted/.*\.ttf$}g } $zip->memberNames())
 		{
@@ -328,8 +328,8 @@ sub _download {
 		env_proxy  => 1,
 	);
 
-	my $file;	   # name of file we download into
-	my $length;	 # total number of bytes to download
+	my $file;	    # name of file we download into
+	my $length;	    # total number of bytes to download
 	my $flength;	# formatted length
 	my $size = 0;   # number of bytes received
 	my $start_t;	# start time of download
